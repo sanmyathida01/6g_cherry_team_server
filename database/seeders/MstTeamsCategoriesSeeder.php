00cf8c3187c757d0b5aee65fb4496fa316b66e8e
HninHninYu <hninhninyu@seattleconsulting.co.jp>
2021-11-16 00:03:02 +0900

    DB名変更

--- database/seeders/MstTeamsCategoriesSeeder.php
+++ database/seeders/MstTeamsCategoriesSeeder.php
@@ -17,23 +17,23 @@ class MstTeamsCategoriesSeeder extends Seeder
     {
         DB::table('mst_teams_categories')->insertOrIgnore([
             [
-                'team_categories_id' => 1,
+                'teams_categories_id' => 1,
                 'teams_categories_name' => "THANKS",
             ],
             [
-                'team_categories_id' => 2,
+                'teams_categories_id' => 2,
                 'teams_categories_name' => "ENGINE",
             ],
             [
-                'team_categories_id' => 3,
+                'teams_categories_id' => 3,
                 'teams_categories_name' => "ALL",
             ],
             [
-                'team_categories_id' => 4,
+                'teams_categories_id' => 4,
                 'teams_categories_name' => "MASTERPIECE",
             ],
             [
-                'team_categories_id' => 5,
+                'teams_categories_id' => 5,
                 'teams_categories_name' => "STOCK",
             ],
         ]);
